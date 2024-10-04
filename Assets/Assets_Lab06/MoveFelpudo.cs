4fcded83e1c22f11d3e700735614cb9dae3e2e69
Thaís Ferreira <thferreirasa@gmail.com>
2024-10-04 11:25:22 -0300

    criação do repositório e base do jogo

---
+++ Assets/Assets_Lab06/MoveFelpudo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFelpudo : MonoBehaviour
+{
+	public Vector3 moveSpeed;
+	public float spawnTime = 2f;
+	public float spawnDelay = 2f;
+
+	// Use this for initialization
+	void Start()
+	{
+		moveSpeed = Vector3.right * Time.deltaTime;
+		InvokeRepeating("ChangeSpeed", spawnDelay, spawnTime);
+	}
+
+	void ChangeSpeed()
+	{
+		moveSpeed = new Vector3(Random.Range(2, 1), 0, 0) * 0.03f;
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.position += moveSpeed;
+	}
+}
---
+++ Assets/Assets_Lab06/MoveFofura.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveFofura: MonoBehaviour
+{
+	public Vector3 moveSpeed;
+	public float spawnTime = 2f;
+	public float spawnDelay = 2f;
+
+	// Use this for initialization
+	void Start()
+	{
+		moveSpeed = Vector3.left * Time.deltaTime;
+		InvokeRepeating("ChangeSpeed", spawnDelay, spawnTime);
+	}
+
+	void ChangeSpeed()
+	{
+		moveSpeed = new Vector3(Random.Range(-1, -2), 0, 0) * 0.03f;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.position += moveSpeed;
+	}
+}
